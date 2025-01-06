61384b463a3d2c747273adf1da65b90e25f547fb
akshajchainani <akshajchainani730@gmail.com>
2025-01-06 15:28:16 +0530

    new

--- pascaltriangle.py
+++ pascaltriangle.py
@@ -1,14 +1,14 @@
 num_rows = 5
-
 triangle = []
 
 for i in range(num_rows):
-    row = [1] * (i + 1)  
-
+    row = [1] * (i + 1)
     for j in range(1, i):
         row[j] = triangle[i - 1][j - 1] + triangle[i - 1][j]
-    
-    triangle.append(row)  
+    triangle.append(row)
+
+max_width = len("   ".join(map(str, triangle[-1])))
 
 for row in triangle:
-    print(row)
+    row_str = "   ".join(map(str, row))
+    print(row_str.center(max_width))
