1ca3234485a4b334a63f295949fd8ce5de956d68
akshajchainani <akshajchainani730@gmail.com>
2025-01-06 15:45:26 +0530

    diamond

---
+++ diamond.py
@@ -0,0 +1,7 @@
+height = 5
+
+for i in range(3, height):
+    print(" " * (height - i - 1) + "*" * (2 * i + 1))
+
+for i in range(height - 2, -1, -1):
+    print(" " * (height - i - 1) + "*" * (2 * i + 1))
