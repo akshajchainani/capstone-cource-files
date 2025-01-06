6b03021d0209e1fac96bd107a8fc24f64a7e8c57
akshajchainani <akshajchainani730@gmail.com>
2025-01-06 12:17:02 +0530

    new

--- first.py
+++ first.py
@@ -1,1 +1,14 @@
-print("hello world")
+a = int(input("enter a number: "))
+sum = 0
+num_digits = len(str(a))
+temp = a
+
+while temp > 0:
+    digit = temp % 10
+    sum += digit ** num_digits
+    temp //= 10
+
+if sum == a:
+    print(f"{a} yes of course")
+else:
+    print(f"{a} no shit")
