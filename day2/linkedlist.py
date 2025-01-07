e8c151924093ff67e4fc0e954a62ab1e2b597a72
akshajchainani <akshajchainani730@gmail.com>
2025-01-07 16:21:35 +0530

    new

--- day2/linkedlist.py
+++ day2/linkedlist.py
@@ -7,7 +7,12 @@ class LinkedList:
     def __init__(self):
         self.head = None
 
-    def create(self, data):
+    def insert_at_beginning(self, data):
+        new_node = Node(data)
+        new_node.next = self.head
+        self.head = new_node
+
+    def insert_at_end(self, data):
         new_node = Node(data)
         if self.head is None:
             self.head = new_node
@@ -17,31 +22,19 @@ class LinkedList:
                 current = current.next
             current.next = new_node
 
-    def insert_at_beginning(self, data):
-        new_node = Node(data)
-        new_node.next = self.head
-        self.head = new_node
-
     def display(self):
         current = self.head
         while current:
             print(current.data, end="  ")
             current = current.next
-        print("none")
+        print("None")
 
+linkedlist = LinkedList()
 
-ll = LinkedList()
+print("Enter 5 values to be inserted at the beginning:")
+for _ in range(5):
+    data = int(input())
+    linkedlist.insert_at_beginning(data)
 
-n = int(input("add at the end: "))
-for _ in range(n):
-    data = int(input("data: "))
-    ll.create(data)
-
-print("adding nodes at the end:")
-ll.display()
-
-data = int(input("insert at the beginning: "))
-ll.insert_at_beginning(data)
-
-print("inserting at the beginning:")
-ll.display()
+print("LinkedList:")
+linkedlist.display()
